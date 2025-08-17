1b062629a9ca578b2af08b6f5bbbbe78fa712e4e
dhanraj080 <dhanraj.gangrade750@gmail.com>
2025-08-18 00:03:40 +0530

    Fixed the CORS error after deploying the backend.

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,8 @@ databaseConnection();
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors()); 
